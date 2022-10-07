f79cf02879d9fb1b2cde1f2d7e84bf0588165b56
Mikhail Zhirnov <lightscore@gmail.com>
2022-10-08 03:05:52 +0400

    init

---
+++ fn/func.go
@@ -0,0 +1,46 @@
+package fn
+
+// functional stuff aka higher-order functions
+
+func Map[T, U any](xs []T, f func(T) U) []U {
+	ys := make([]U, 0, len(xs))
+	for _, x := range xs {
+		ys = append(ys, f(x))
+	}
+	return ys
+}
+
+func Filter[T any](xs []T, f func(T) bool) []T {
+	ys := make([]T, 0)
+	for _, x := range xs {
+		if f(x) {
+			ys = append(ys, x)
+		}
+	}
+	return ys
+}
+
+func Reduce[T, U any](init T, xs []U, f func(T, U) T) T {
+	for _, x := range xs {
+		init = f(init, x)
+	}
+	return init
+}
+
+func Any[T any](xs []T, f func(T) bool) bool {
+	for _, x := range xs {
+		if f(x) {
+			return true
+		}
+	}
+	return false
+}
+
+func All[T any](xs []T, f func(T) bool) bool {
+	for _, x := range xs {
+		if !f(x) {
+			return false
+		}
+	}
+	return true
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lightscore/generic_tools
+
+go 1.19
